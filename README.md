3c1f4246fb2e4a61a3d54a8b398d0babcb58b589
three-cubed <72698356+three-cubed@users.noreply.github.com>
2021-03-31 19:00:38 +0100

    reinitialising repository

---
+++ README.md
@@ -0,0 +1,7 @@
+### About the projects app
+
+This little application is designed to give an overview of my projects.
+
+The repository for the application also gives an incidental example, albeit a short and simple one, of my serverside programming.
+
+Name/contact information is kept in the environment variables and is not visible in the repository.
---
+++ server-router.js
@@ -0,0 +1,31 @@
+if (process.env.NODE_ENV !== 'production') {
+        require('dotenv').config({ path: './.env' })
+}
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+const favicon = require('serve-favicon');
+
+app.set('view engine', 'ejs');
+app.use(favicon('./favicon.png'));
+app.use(express.static('pics'));
+
+// const info = require('./info.js');
+
+app.get('/', (req, res) => {
+        res.render('projects', {
+                nameComment: process.env.nameComment,
+                contactComment: process.env.contactMe,
+        });
+});
+
+app.get('/nameblind', (req, res) => {
+        res.render('projects', {
+                nameComment: '',
+                contactComment: process.env.noContact,
+        });
+});
+
+app.listen(PORT);
+console.log('listening to port: ' + PORT);
